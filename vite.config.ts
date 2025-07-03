e20c9eca2b5bad72e9cc87a7992d88cf968156a3
Alan Kovac <kraprie.wow3@gmail.com>
2025-07-03 20:40:22 +0200

    add vite cfg0

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/spa-core-ga-cookies/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
